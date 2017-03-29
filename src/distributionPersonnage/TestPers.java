c5bdfa93355a3845f60d29c30e509975ad3bd181
StraetenMax <maxstraeten@yahoo.com>
2017-03-29 12:59:53 +0200

    mettre en place le main

---
+++ src/distributionPersonnage/TestPers.java
@@ -0,0 +1,12 @@
+package distributionPersonnage;
+
+public class TestPers {
+
+	public static void main(String[] args) {
+		Personnage personnage = new Personnage();
+		Personnage personnage1 = new Personnage("humain", 180,80);
+
+		
+	}
+
+}
