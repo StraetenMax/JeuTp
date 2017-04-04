c506fc4ef6f1ede063771948c07a0b7db61af7a6
StraetenMax <maxstraeten@yahoo.com>
2017-04-04 13:42:40 +0200

    création du constructeur et des attributs

--- src/distributionPersonnage/PersonnageJoueurMedecin.java
+++ src/distributionPersonnage/PersonnageJoueurMedecin.java
@@ -1,5 +1,19 @@
 package distributionPersonnage;
 
-public class PersonnageJoueurMedecin {
-
+public class PersonnageJoueurMedecin extends PersonnageJoueur{
+	//attribut - propriétés
+		private int nbMedicaments;
+		
+	// constructeurs
+		public PersonnageJoueurMedecin(){		
+			super();
+			nbMedicaments = 0;
+			System.out.println("je suis le joueur Médecin");
+		}
+		
+		public PersonnageJoueurMedecin(String pRace, int pTaille, int pPoids, String pNom, int pNbMedicaments){
+			super(pRace, pTaille, pPoids, pNom);
+			nbMedicaments = pNbMedicaments;
+			System.out.println("je suis le joueur Médecin 2");
+		}
 }
